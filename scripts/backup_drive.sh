8b579891aee3da93b4f23621c768aacc3b2ac627
gus <gus@ljohnson.domain>
2024-10-09 19:23:39 -0600

    mas changess!

--- scripts/backup_drive.sh
+++ scripts/backup_drive.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # -anv means archive, dry run, and backup (keep one copy of any pre-existing files)
 # add -n to do a dry run
-echo "" >> /home/gus/logs/backup_drive.log
-echo "==============================================" >> /home/gus/logs/backup_drive.log
+echo "=============================================="
 echo $(date) >> /home/gus/logs/backup_drive.log
-sudo rsync -avb --suffix .$(date +%Y%m%d) /mnt/bendo4/SHARE/ /mnt/ben10 >> /home/gus/logs/backup_drive.log
+sudo rsync -avb --suffix .$(date +%Y%m%d) /mnt/bendo4/SHARE/ /mnt/ben10/SHARE
