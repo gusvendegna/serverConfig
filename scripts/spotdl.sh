4ef9b6ddbf8b2b95ec5326d27a59f58e8f9c56ac
gus <gus@mjohnson.domain>
2025-06-19 16:33:24 -0600

    adde tld proxy all up in this ish

--- scripts/spotdl.sh
+++ scripts/spotdl.sh
@@ -5,12 +5,14 @@ MUSIC_DIR="/mnt/bendo4/SHARE/media/music"
 
 # List of playlists (name and URL)
 playlists=(
-    "dump_playlist https://open.spotify.com/playlist/2NOX2ZaxJ6NKfVDs8EWQ67?si=929cd344cbc44b46"
-    "late_nights https://open.spotify.com/playlist/1K91E1HtWp0PKjgMtuovHO?si=baa550fa38c246e4"
-    "notaphase https://open.spotify.com/playlist/1LXHvZJhuMNezUNZI9ub9U?si=66bff1363d0b4a93"
-    "unstable_women https://open.spotify.com/playlist/7gnhk765qTbXoGkGHTN35u?si=629a84ff28fb4502"
-    "yupee https://open.spotify.com/playlist/2qqjJ34mOAaWEIx8fNBDwB?si=5be4d88a693743c2"
-    "en_el_garaje https://open.spotify.com/playlist/6chwePfM44HJ9AYY3zlWuy?si=7fed17b23de1495d"
+    # "dump_playlist https://open.spotify.com/playlist/2NOX2ZaxJ6NKfVDs8EWQ67?si=929cd344cbc44b46"
+    # "late_nights https://open.spotify.com/playlist/1K91E1HtWp0PKjgMtuovHO?si=baa550fa38c246e4"
+    # "notaphase https://open.spotify.com/playlist/1LXHvZJhuMNezUNZI9ub9U?si=66bff1363d0b4a93"
+    # "unstable_women https://open.spotify.com/playlist/7gnhk765qTbXoGkGHTN35u?si=629a84ff28fb4502"
+    # "yupee https://open.spotify.com/playlist/2qqjJ34mOAaWEIx8fNBDwB?si=5be4d88a693743c2"
+    # "en_el_garaje https://open.spotify.com/playlist/6chwePfM44HJ9AYY3zlWuy?si=7fed17b23de1495d"
+    "b_t_p https://open.spotify.com/playlist/4iLrgpUfRpBj45tvnEa0lC"
+    "midwest_emo https://open.spotify.com/playlist/6iqHIszYUV3wHxQZmRZxWA"
 )
 
 for entry in "${playlists[@]}"; do
