9121848d0d2a4ba74004b3aeb66656f693ac85e4
gus <gus@mjohnson.domain>
2025-11-01 19:10:40 -0600

    souls have been found and healtchchecks

---
+++ scripts/health_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+URL="https://hc-ping.com/${PING_KEY}/${HOSTNAME}-up"
+
+while true; do
+  curl -fsS -m 10 "$URL" > /dev/null
+  echo "Pinged at $(date)"
+  sleep 300  # 5 minutes
+done
+
